4d35006ad5327613d9f980c132a4ac42fd6f52b2
Shenghui Hong <shenghui.hong@studio.unibo.it>
2025-12-01 13:06:33 +0100

    Modify ArithmeticService.process()

--- 63-exceptions/src/main/java/it/unibo/exceptions/arithmetic/ArithmeticService.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/arithmetic/ArithmeticService.java
@@ -55,43 +55,47 @@ public final class ArithmeticService {
      * @return the result of the process
      */
     public String process() {
-        if (commandQueue.isEmpty()) {
-            System.out.println("No commands sent, no result available");
-        }
-        while (commandQueue.size() != 1) {
-            final var nextMultiplication = commandQueue.indexOf(TIMES);
-            final var nextDivision = commandQueue.indexOf(DIVIDED);
-            final var nextPriorityOp = nextMultiplication >= 0 && nextDivision >= 0
-                ? min(nextMultiplication, nextDivision)
-                : max(nextMultiplication, nextDivision);
-            if (nextPriorityOp >= 0) {
-                computeAt(nextPriorityOp);
-            } else {
-                final var nextSum = commandQueue.indexOf(PLUS);
-                final var nextMinus = commandQueue.indexOf(MINUS);
-                final var nextOp = nextSum >= 0 && nextMinus >= 0
-                    ? min(nextSum, nextMinus)
-                    : max(nextSum, nextMinus);
-                if (nextOp != -1) {
-                    if (commandQueue.size() < 3) {
-                        System.out.println("Inconsistent operation: " + commandQueue);
+        try {
+            if (commandQueue.isEmpty()) {
+                throw new IllegalStateException("No commands sent, no result available");
+            }
+            while (commandQueue.size() != 1) {
+                final var nextMultiplication = commandQueue.indexOf(TIMES);
+                final var nextDivision = commandQueue.indexOf(DIVIDED);
+                final var nextPriorityOp = nextMultiplication >= 0 && nextDivision >= 0
+                    ? min(nextMultiplication, nextDivision)
+                    : max(nextMultiplication, nextDivision);
+                if (nextPriorityOp >= 0) {
+                    computeAt(nextPriorityOp);
+                } else {
+                    final var nextSum = commandQueue.indexOf(PLUS);
+                    final var nextMinus = commandQueue.indexOf(MINUS);
+                    final var nextOp = nextSum >= 0 && nextMinus >= 0
+                        ? min(nextSum, nextMinus)
+                        : max(nextSum, nextMinus);
+                    if (nextOp != -1) {
+                        if (commandQueue.size() < 3) {
+                            System.out.println("Inconsistent operation: " + commandQueue);
+                        }
+                        computeAt(nextOp);
+                    } else if (commandQueue.size() > 1) {
+                        System.out.println("Inconsistent state: " + commandQueue);
                     }
-                    computeAt(nextOp);
-                } else if (commandQueue.size() > 1) {
-                    System.out.println("Inconsistent state: " + commandQueue);
                 }
             }
-        }
-        final var finalResult = commandQueue.get(0);
-        final var possibleException = nullIfNumberOrException(finalResult);
-        if (possibleException != null) {
-            System.out.println("Invalid result of operation: " + finalResult);
-        }
-        return finalResult;
+            final var finalResult = commandQueue.get(0);
+            final var possibleException = nullIfNumberOrException(finalResult);
+            if (possibleException != null) {
+                throw new IllegalStateException("Invalid result of operation: " + finalResult);
+            }
+            return finalResult;
         /*
          * The commandQueue should be cleared, no matter what, when the method exits
          * But how?
          */
+        } finally {
+            commandQueue.clear();
+        }
     }
 
     private void computeAt(final int operatorIndex) {
